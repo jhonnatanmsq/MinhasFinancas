5611bf728a8a16fb870b224938040bea6f647960
Jhonnatan Mesquita <jhonnatanmesquita@gmail.com>
2020-02-10 22:23:16 -0300

    Ajustes iniciais

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,15 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  alphavantageUrl : 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY',
+  alphavantageKEY : 'SUA_CHAVE_AQUI',
+  firebase: {
+    apiKey: 'SUA_CHAVE_AQUI',
+    authDomain: 'SUA_CHAVE_AQUI',
+    databaseURL: 'SUA_CHAVE_AQUI',
+    projectId: 'SUA_CHAVE_AQUI',
+    storageBucket: 'SUA_CHAVE_AQUI',
+    messagingSenderId: 'SUA_CHAVE_AQUI',
+    appId: 'SUA_CHAVE_AQUI',
+    measurementId: 'SUA_CHAVE_AQUI'
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
